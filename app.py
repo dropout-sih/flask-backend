1a549bab8038590de56b2443dc53fcc6a585a92a
Rahul Arulkumaran <rahuldravid313@gmail.com>
2019-03-02 10:27:33 +0530

    debug callback function

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from bokeh.embed import components
-from bokeh.plotting import figure, curdoc, ColumnDataSource
+from bokeh.plotting import figure, curdoc, ColumnDataSource, show
 from bokeh.resources import INLINE
 from bokeh.util.string import encode_utf8
 from bokeh.models import CustomJS, LabelSet, Slider
 from bokeh.models.widgets import Slider
 from bokeh.models.layouts import WidgetBox, Row
+from bokeh.layouts import row, widgetbox
 from flask import Flask, render_template, flash, request, redirect, url_for, session
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import sendgrid
@@ -22,89 +23,52 @@ external = df['Normalised_x'].tolist()
 internal = df['Normalised_y'].tolist()
 names = df['Country'].tolist()
 
-source = dict()
-
-'''def callback(source=source):
-	data = source.data
-	print("httt")
-	year_dyn = cb_obj.year.value  # cb_obj.get('a_slider')
-	x, y = data['external'], data['internal']
-
-	for i in range(0,len(y)):
-		y[i] = y[i] + 0.05
-
-	source.change.emit()
-'''
-
-def updatePage2(attrname, old, new):
-	print("asdsaD")
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/visualize", methods=['GET', 'POST'])
 def index():
-	form = CompetitionForm(request.form)
+	form = CompetitionForm()
 	js_resources = INLINE.render_js()
 	css_resources = INLINE.render_css()
-	global source
 	source = ColumnDataSource(data=dict(external=external, internal=internal,names=names))
-	fig = figure(plot_width=1000, plot_height=600)
-	fig.scatter('external','internal', source=source, marker="inverted_triangle", size=5,line_color="navy", fill_color="green")
 
-	updatePage = CustomJS(args=dict(source=source), code="""
+	callback = CustomJS(args=dict(source=source), code="""
 		var data = source.data;
-		var year = year.value;
+		var val = year.value;
 		var x = data['external'];
 		var y = data['internal'];
 		for (var i = 0; i < x.length; i++) {
-		    y[i] = x[i] + year;
+		    x[i] = x[i] + 0.05;
 		}
+		source.change.emit();
 	""")
-	fig.xaxis[0].axis_label = 'External Factors'
-	fig.yaxis[0].axis_label = 'Internal Factors'
+
+
+	fig = figure(plot_width=1000, plot_height=600)
+	fig.scatter('external','internal', source=source, marker="inverted_triangle", size=5,line_color="navy", fill_color="green", alpha=0.6)
+	fig.xaxis[0].axis_label = 'External Index'
+	fig.yaxis[0].axis_label = 'Internal Index'
 	labels = LabelSet(x='external', y='internal', text='names', level='glyph', x_offset=5, y_offset=5, source=source, render_mode='canvas')
 	fig.add_layout(labels)
-	year = Slider(title="Year Start", value=2000, start=2000, end=2030, step=1, width=500)#, callback=updatePage2)
-	#updatePage.args["year"] = year
-	year.on_change('value', updatePage)
+	year = Slider(title="Year", value=2000, start=2000, end=2030, step=1, width=250, callback=callback)
+	callback.args["year"] = year
 
-	script, div = components(fig)
-	script_sli, div_sli = components(year)
-	#print(script, div, script_sli, div_sli)
-	html = render_template(
-		'index.html',
-		plot_script=script,
-		plot_div=div,
-		js_resources=js_resources,
-		css_resources=css_resources,
-		form=form,
-		slider_script=script_sli,
-		slider_div=div_sli,
+	layout = row(
+	    fig,
+	    widgetbox(year),
 	)
 
-	if(request.method == 'POST'):
-		if(form.validate()):
-			fig = figure(plot_width=600, plot_height=600)
-			fig.vbar(x=[1, 2, 3, 4], width=0.5, bottom=0, top=[1.7, 2.2, 4.6, 3.9], color='navy')
+	script, div = components(layout)
+	html = render_template(
+		'index.html',
+		form=form,
+		layout_script=script,
+		layout_div=div,
+		js_resources=js_resources,
+		css_resources=css_resources,
+	)
 
-			#print(components(fig))
-			# grab the static resources
-			year = Slider(title="Year Start", value=2000, start=2000, end=2030, step=1, width=500)
-			# render template
-			script, div = components(year)
-			script_sli, div_sli = components(year)
-
-			html = render_template(
-			    'index.html',
-			    plot_script=script,
-			    plot_div=div,
-			    js_resources=js_resources,
-			    css_resources=css_resources,
-				form=form,
-				slider_script=script_sli,
-				slider_div=div_sli,
-			)
-			return encode_utf8(html)
 	return encode_utf8(html)
 
 
